8660607ab184d584c05c74eadd37d784cd27cc25
Ubuntu <ubuntu@ip-172-31-23-14.us-east-2.compute.internal>
2020-03-23 00:32:25 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,8 +14,8 @@ class MoviesController < ApplicationController
     # @all_ratings = Movie.distinct.pluck(:rating)
     @all_ratings = Movie.ratings
     @sort = params[:sort]
-    params[:ratings].nil? ? t_param = @all_ratings : t_param = params[:ratings].keys
-    @movies = Movie.where(rating: t_param).order(@sort)
+    params[:ratings].nil? ? @t_param = @all_ratings : @t_param = params[:ratings].keys
+    @movies = Movie.where(rating: @t_param).order(@sort)
 
   end
 
