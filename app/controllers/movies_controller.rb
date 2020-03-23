9f81d647a6c85b828cf5512e4e483ad26714c471
Ubuntu <ubuntu@ip-172-31-23-14.us-east-2.compute.internal>
2020-03-23 02:14:12 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,10 +13,17 @@ class MoviesController < ApplicationController
   def index
     # @all_ratings = Movie.distinct.pluck(:rating)
     @all_ratings = Movie.ratings
-    @sort = params[:sort]
-    params[:ratings].nil? ? @t_param = @all_ratings : @t_param = params[:ratings].keys
-    @movies = Movie.where(rating: @t_param).order(@sort)
-
+    @sort = params[:sort] || session[:sort]
+    session[:ratings] = session[:ratings] || {'G'=>'', 'PG'=>'', 'PG-13'=>'','R'=>''}
+    # params[:ratings].nil? ? @t_param = @all_ratings : @t_param = params[:ratings].keys
+    @t_param = params[:ratings] || session[:ratings]
+    session[:sort] = @sort
+    session[:ratings] = @t_param
+    @movies = Movie.where(rating: session[:ratings].keys).order(session[:sort])
+    if (params[:sort].nil? and !(session[:sort].nil?)) or (params[:ratings].nil? and !(session[:ratings].nil?))
+      flash.keep
+      redirect_to movies_path(sort: session[:sort], ratings: session[:ratings])
+    end
   end
 
   def new
