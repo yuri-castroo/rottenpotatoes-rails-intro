2c478bfb613fe7085fa224fb24239981de19d5e2
Yuri Castro <castroyuri09@gmail.com>
2020-04-28 22:01:26 -0500

    can sort now

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,6 +20,37 @@ class MoviesController < ApplicationController
     session[:sort] = @sort
     session[:ratings] = @t_param
     @movies = Movie.where(rating: session[:ratings].keys).order(session[:sort])
+    
+
+    # @movies = Movie.all
+    # @all_ratings = Movie.ratings
+
+    # if params[:sort] != nil
+    #   @sort_by = params[:sort]
+    #   session[:sort] = params[:sort]
+    # elsif session[:sort] != nil
+    #   @sort_by = session[:sort]
+    # else
+    #   @sort_by = 'id'
+    # end
+    
+    if params[:ratings] != nil
+      @rating_filter = params[:ratings]
+      session[:ratings] = params[:ratings]
+    elsif session[:ratings] != nil
+      @rating_filter = session[:ratings]
+    else
+      @rating_filter = nil
+    end
+    
+    # if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
+    #   session[:ratings] = @rating_filter
+    #   session[:sort] = @sort_by
+    #   flash.keep
+    #   redirect_to movies_path(:sort => @sort_by, :ratings => @rating_filter)
+    #   return
+    # end
+
     if (params[:sort].nil? and !(session[:sort].nil?)) or (params[:ratings].nil? and !(session[:ratings].nil?))
       flash.keep
       redirect_to movies_path(sort: session[:sort], ratings: session[:ratings])
